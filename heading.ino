8ac17970aa37252ba4a92070cebb8a0d7f6e3eeb
Muhammad Habibullah <mhabibullah14@yahoo.com>
2020-02-16 19:28:42 +0700

    simplify ID heading

--- heading.ino
+++ heading.ino
@@ -1,11 +1,11 @@
 
 String getMataAngin(double course) {
    static const char* directions[] = {
-        "Utara", "Utara - Timur Laut", "Timur Laut", "Timur Laut", 
-        "Timur", "Timur - Tenggara", "Tenggara", "Selatan Tenggara", 
-        "Selatan", "Selatan Daya", "Barat Daya", "Barat Daya", 
-        "Barat", "Barat Laut", "Barat Laut", "Utara - Barat Laut"
+        "Utara", "Timur Laut", 
+        "Timur", "Tenggara", 
+        "Selatan", "Barat Daya", 
+        "Barat", "Barat Laut", 
     };
-    int direction = (int)((course + 11.25f) / 22.5f);
-    return directions[direction % 16];
+    int direction = (int)((course + 22.5f) / 45f);
+    return directions[direction % 8];
 }
